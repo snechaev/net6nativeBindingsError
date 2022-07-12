05b52c00f8dc146d89d58f41603610e8acc570be
Sergey Nechaev <6499856+snechaev@users.noreply.github.com>
2022-07-12 18:15:25 +0700

    initial

---
+++ mono/app/AppDelegate.cs
@@ -0,0 +1,36 @@
+﻿using AppKit;
+using Foundation;
+using ScriptingBridge;
+using NativeBindingsLib;
+
+namespace app
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : NSApplicationDelegate
+	{
+		public AppDelegate ()
+		{
+		}
+
+		public override void DidFinishLaunching (NSNotification notification)
+		{
+			// Insert code here to initialize your application
+
+			var app = SBApplication.GetApplication<FinderApplication>("com.apple.finder");
+			var version = app.Version;
+			using (var alert = new NSAlert())
+			{
+				alert.InformativeText = version.ToString();
+				alert.RunModal();
+			}
+
+
+		}
+
+		public override void WillTerminate (NSNotification notification)
+		{
+			// Insert code here to tear down your application
+		}
+	}
+}
+
---
+++ net6/app/Main.cs
@@ -0,0 +1,5 @@
+using app;
+
+// This is the main entry point of the application.
+NSApplication.Init ();
+NSApplication.Main (args);
